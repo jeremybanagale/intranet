69a85c25b7df5aceb651f2af2b496cf0f24ed0ac
Jigno Alfred V. Venezuela <jignoalfred.venezuela@live.com>
2017-01-30 18:57:12 +0800

    Added favicon. START: Authenticate with passport.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//app dependencies
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -15,7 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,11 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-
-
-// app.get('/intern', function(req, res){
-//    res.render('intern.jade');
-// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
