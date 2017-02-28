d8fa3a83be3a4a39b73812f5bdb777012c3afe2a
jeremybanagale <jbanagale.bos@gmail.com>
2017-02-28 14:12:05 +0800

    Update topnav.js

--- public/javascripts/topnav.js
+++ public/javascripts/topnav.js
@@ -1,47 +1,41 @@
-$(window).scroll(function() {
-  var navbarColor = "225, 225, 225"
-  var smallLogoHeight = $('.small-logo').height();
-  var bigLogoHeight = $('.big-logo').height();
-  var navbarHeight = $('.navbar').height();
+var mywindow = $(window);
+var mypos = mywindow.scrollTop();
+var up = false;
+var newscroll;
 
-  var smallLogoEndPos = 0;
-  var smallSpeed = (smallLogoHeight / bigLogoHeight);
-
-  var ySmall = ($(window).scrollTop() * smallSpeed);
-
-  var smallPadding = navbarHeight - ySmall;
-  if (smallPadding > navbarHeight) { smallPadding = navbarHeight; }
-  if (smallPadding < smallLogoEndPos) { smallPadding = smallLogoEndPos; }
-  if (smallPadding < 0) { smallPadding = 0; }
-
-  $('.small-logo-container ').css({ "padding-top": smallPadding});
-  
-  var navOpacity = ySmall / smallLogoHeight;
-  if  (navOpacity > 1) { navOpacity = 1; }
-  if (navOpacity < 0 ) { navOpacity = 0; }
-  var navBackColor = 'rgba(' + navbarColor + ',' + navOpacity + ')';
-  $('.navbar').css({"background-color": navBackColor});
-
-  var shadowOpacity = navOpacity * 0.4;
-  if ( ySmall > 1) {
-    $('.navbar').css({"box-shadow": "0 3px 3px rgba(0,0,0," + shadowOpacity + ")"});
-  } else {
-    $('.navbar').css({"box-shadow": "none"});
-  }
-});
-
-$(document).ready(function() {
-    $('.navbar a.dropdown-toggle').on('click', function(e) {
-        var $el = $(this);
-        var $parent = $(this).offsetParent(".dropdown-menu");
-        $(this).parent("li").toggleClass('open');
-
-        if(!$parent.parent().hasClass('nav')) {
-            $el.next().css({"top": $el[0].offsetTop, "left": $parent.outerWidth() - 4});
-        }
-
-        $('.nav li.open').not($(this).parents("li")).removeClass("open");
-
-        return false;
+mywindow.scroll(function() {
+  newscroll = mywindow.scrollTop();
+  var navbarColor = "0, 42, 67";
+  var navOpacity = 0;
+  var navBackColor;
+  var smallLogoBarColor = "0, 42, 67"
+  var smallLogoBackColor;
+  if (newscroll > mypos && !up) {
+    navOpacity = 1;
+    navBackColor = 'rgba(' + navbarColor + ',' + navOpacity + ')';
+    //smallLogoBackColor = 'rgb(' + smallLogoBarColor + ')';
+    $('.navbar').css({
+      "background-color": navBackColor
     });
+    $(".navbar").animate({
+      padding: '0px'
+    });
+    $(".small-logo-container").fadeTo(500, 1);
+    up = !up;
+
+  } else if (newscroll == 0 && up) {
+    navOpacity = 0;
+    navBackColor = 'rgba(' + navbarColor + ',' + navOpacity + ')';
+    $('.navbar').css({
+      "background-color": navBackColor
+    });
+    $(".navbar").animate({
+      padding: '10px'
+    });
+    $(".small-logo-container").fadeTo(500, 0);
+    up = !up;
+  }
+
+  mypos = newscroll;
+  console.log(mypos);
 });
