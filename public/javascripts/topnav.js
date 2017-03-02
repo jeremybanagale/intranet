eb49527ef9adb61bd5d42a72b0557700cd0fce3a
jeremybanagale <jbanagale.bos@gmail.com>
2017-03-02 11:53:10 +0800

    Update topnav.js
    jquery of slider

--- public/javascripts/topnav.js
+++ public/javascripts/topnav.js
@@ -1,4 +1,22 @@
-
+$(document).ready(function(){
+  
+  $(".Modern-Slider").slick({
+    autoplay:true,
+    autoplaySpeed:10000,
+    speed:600,
+    slidesToShow:1,
+    slidesToScroll:1,
+    pauseOnHover:false,
+    dots:true,
+    pauseOnDotsHover:true,
+    cssEase:'linear',
+   // fade:true,
+    draggable:false,
+    prevArrow:'<button class="PrevArrow fa fa-chevron-right" aria-hidden="true"></button>',
+    nextArrow:'<button class="NextArrow fa fa-chevron-right" aria-hidden="true"></button>', 
+  });
+  
+})
 var mywindow = $(window);
 var mypos = mywindow.scrollTop();
 var up = false;
